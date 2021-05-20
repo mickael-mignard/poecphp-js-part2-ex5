dca773a2db5f6ca8bf3ea674582a760318020350
Mickael Mignard <mignard.mickael@gmail.com>
2021-05-20 15:14:20 +0200

    Fonction pour changer 5 images

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+function changerImage() {
+    let racineNom = "images/image";
+    let container = document.getElementsByClassName("container")[0];
+    container.addEventListener("mouseover", function () {
+        let images = container.getElementsByTagName("img"); 
+        for (let i = 1; i < images.length+1; i++) {
+            let image = document.getElementById("image" + i);
+            image.addEventListener("mouseover", function () {
+                image.setAttribute("src", racineNom + i + "_2" + ".jpg");
+            })
+        }  
+    })
+}
+
+
+
