8ec8c76d2c0032d55171f6a23042a8362f299f10
Mickael Mignard <mignard.mickael@gmail.com>
2021-05-26 10:04:08 +0200

    Simplification du code

--- js/script.js
+++ js/script.js
@@ -1,21 +1,10 @@
 /**
- * Change image when mouse enters
+ * Change image when mouse enters 
  */
- (function(){
+let images = document.querySelectorAll('img');
 
-    let images = document.querySelectorAll('img');     
-
-    images.forEach( elt =>  {        
-       
-        elt.addEventListener('mouseenter', function changeImage(){
-            let attributeSrc = this.getAttribute('src');
-            let rootPath = attributeSrc.substring(0, attributeSrc.length-4);            
-            this.setAttribute('src', rootPath + '_2.jpg');  
-            this.removeEventListener('mouseenter', changeImage);          
-        })
-
-    });       
-}());
-
-
-
+images.forEach(elt => {
+  elt.addEventListener('mouseover', function () {
+    elt.src = 'images/' + elt.getAttribute('id')+ '_2.jpg';
+  });
+});
